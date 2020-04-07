c986b66e171811026d742f59ac690cdf8ed88331
Artur Pilch <artugam@gmail.com>
2020-04-07 13:12:39 +0200

    finished users module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,9 @@
 //users/users.service.ts
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import {UserEntity} from './user.entity';
 import {UserInput} from './dto/user.input';
 import {UsersRepository} from './users.repository';
+import {Not, Equal} from 'typeorm';
 
 @Injectable()
 export class UsersService {
@@ -12,12 +13,25 @@ export class UsersService {
     }
 
     async create(userInput: UserInput): Promise<UserEntity>{
+        const anotherUserWithGivenMail = await this.usersRepository.findOne({email: userInput.email});
+        if(anotherUserWithGivenMail) {
+            throw new BadRequestException('Another user with given email already exists');
+        }
         const user = new UserEntity();
         this.fillUser(userInput, user);
         return this.usersRepository.save(user);
     }
 
     async update(id: number, userInput: UserInput): Promise<UserEntity>{
+        const anotherUserWithGivenMail = await this.usersRepository.findOne({
+            where: {
+                id: Not(Equal(id)),
+                email: Equal(userInput.email)
+            }
+        });
+        if(anotherUserWithGivenMail) {
+            throw new BadRequestException('Another user with given email already exists');
+        }
         const user = await this.usersRepository.findOneOrFail({id});
         this.fillUser(userInput, user);
         return this.usersRepository.save(user);
